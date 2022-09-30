276707cebf6b52e973da986a558b0745d7c8c5b7
ifan4 <ifana.andri@upi.edu>
2022-09-30 22:42:01 +0700

    update api

---
+++ src/utils/axios-utils.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import Cookies from 'js-cookie'
+
+
+const client = axios.create( {
+    baseURL: 'sisis.ifandri.com',
+} )
+
+export const request = ( {...options},token=true ) => {
+    
+    if (token){
+        client.defaults.headers.common.Authorization = `Bearer ${Cookies.get('accessToken')}`
+        console.log('masuk token true0');
+    }
+
+
+    const onSuccess = response => response;
+    const onError = error => {
+        return error
+    }
+
+    return client(options)
+}
